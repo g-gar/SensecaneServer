4a9be74fc46f03bec046398baaf65c6ba9d8c8f3
ggar <g.gar.fern@hotmail.com>
2020-05-14 01:22:00 +0200

    random

--- src/main/java/com/magc/sensecane/server/facade/dao/AbstractDaoUtil.java
+++ src/main/java/com/magc/sensecane/server/facade/dao/AbstractDaoUtil.java
@@ -1,13 +1,12 @@
 package com.magc.sensecane.server.facade.dao;
 
-import java.util.function.Supplier;
-
 import com.magc.sensecane.framework.container.Container;
 import com.magc.sensecane.framework.dao.Dao;
 import com.magc.sensecane.framework.dao.DaoContainer;
+import com.magc.sensecane.framework.facade.util.AbstractFacadeUtil;
 import com.magc.sensecane.framework.model.database.TableEntity;
 
-public abstract class AbstractDaoUtil {
+public abstract class AbstractDaoUtil extends AbstractFacadeUtil {
 	
 	protected final Container container;
 	
@@ -19,29 +18,5 @@ public abstract class AbstractDaoUtil {
 		return container.get(DaoContainer.class).get(clazz);
 	}
 	
-	public <T> boolean isInstance(Class<T> clazz, Object param) {
-		return param.getClass().isInstance(clazz);
-	}
 	
-	public <R> R tryOr(Supplier<R>...fns) {
-		R result = null;
-		
-		boolean success = false;
-		int i = 0;
-		while (!success && i < fns.length) {
-			Supplier<R> fn = fns[i++];
-			try {
-				result = fn.get();
-				success = true;
-			} catch (Exception e) {
-				
-			}
-		}
-		
-		if (!success) {
-			throw new RuntimeException(new Exception(String.format("[%s] Couldn't find a successful method to apply\"", this.getClass())));
-		}
-		
-		return result;
-	}
 }
