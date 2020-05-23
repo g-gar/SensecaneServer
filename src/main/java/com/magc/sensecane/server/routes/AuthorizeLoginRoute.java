a620ffc51e4674ff74a2aa22ef3b8e9e049b6dac
ggar <g.gar.fern@hotmail.com>
2020-05-23 22:31:50 +0200

    fixed authorize login route

--- src/main/java/com/magc/sensecane/server/routes/AuthorizeLoginRoute.java
+++ src/main/java/com/magc/sensecane/server/routes/AuthorizeLoginRoute.java
@@ -9,7 +9,6 @@ import com.magc.sensecane.framework.model.json.PreSerializedJson;
 import com.magc.sensecane.framework.spark.AbstractPostRoute;
 import com.magc.sensecane.server.facade.DaoFacade;
 import com.magc.sensecane.server.facade.ResponseModelFacade;
-import com.magc.sensecane.server.model.User;
 import com.magc.sensecane.server.model.database.UserTable;
 
 import spark.Request;
@@ -23,19 +22,25 @@ public class AuthorizeLoginRoute extends AbstractPostRoute<Object> {
 
 	@Override
 	public PreSerializedJson<Object> serve(Request request, Response response) throws Exception {
-		Map<String, String> params = super.getParams(request, "username", "password");
-		String username = params.get("username");
-		String password = params.get("password");
-		
-		Dao<UserTable> udao = container.get(DaoContainer.class).get(UserTable.class);
-		Object user = udao.findAll()
-				.stream()
-				.map(e->DaoFacade.getUserInfo(e.getId()))
-				.filter(e -> e.getUsername().equals(username) && e.getPassword().equals(password))
-				.limit(1)
-				.map(ResponseModelFacade::createUser)
-				.findFirst().orElse(null);
-		return new PreSerializedJson<Object>(user, "id", "username", "dni", "firstName", "lastName", "type");
+		PreSerializedJson<Object> result = null;
+		try {
+			Map<String, String> params = super.getParams(request, "username", "password");
+			String username = params.get("username");
+			String password = params.get("password");
+			
+			Dao<UserTable> udao = container.get(DaoContainer.class).get(UserTable.class);
+			result = udao.findAll()
+					.stream()
+					.map(e->DaoFacade.getUserInfo(e.getId()))
+					.filter(e -> e.getUsername().equals(username) && e.getPassword().equals(password))
+					.limit(1)
+					.map(ResponseModelFacade::createUser)
+					.map(e -> new PreSerializedJson<Object>(e, "id", "username", "dni", "firstName", "lastName", "type"))
+					.findFirst().orElse(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 }
